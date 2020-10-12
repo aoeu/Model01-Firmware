9d4f0029893ebb78a7973553055f880d754f291f
Travis snɯǝᗡɔW <aoeu@users.noreply.github.com>
2020-10-12 14:27:43 -0400

    Swap control and command keys for macOS usage

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -226,16 +226,16 @@ KEYMAPS(
   [PRIMARY] = KEYMAP_STACKED
   (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_LeftGui, Key_A, Key_S, Key_D, Key_F, Key_G,
+   Key_LeftControl, Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_LeftAlt, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftShift, Key_LeftBracket,
+   Key_LeftGui, Key_Backspace, Key_LeftShift, Key_LeftBracket,
    ShiftToLayer(FUNCTION),
 
    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_LeftBracket,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_RightGui, Key_N, Key_M, Key_Comma, Key_Period, Key_Slash, Key_RightBracket,
-   Key_RightAlt, Key_RightShift, Key_Spacebar, Key_RightControl,
+   Key_RightControl, Key_N, Key_M, Key_Comma, Key_Period, Key_Slash, Key_RightBracket,
+   Key_RightAlt, Key_RightShift, Key_Spacebar, Key_RightGui,
    ShiftToLayer(FUNCTION)),
 #else
 
